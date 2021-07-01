96fbe958cd2c5803fe8add04ea618a3bc08e9a08
Arex <arexchu9@gmail.com>
2021-07-01 22:02:29 +0800

    delete python3 command

--- init.el
+++ init.el
@@ -60,7 +60,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elpy-rpc-python-command "python3")
  '(package-selected-packages '(elpy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
